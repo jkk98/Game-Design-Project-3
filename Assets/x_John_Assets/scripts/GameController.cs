a136ade2b8887ba2e50feea7294d83aa8fa84792
jkk98 <jkk98@msstate.edu>
2016-04-06 14:36:28 -0500

    Started Work on Game Timer
    Set up a basic coroutine for a global game timer

--- Assets/x_John_Assets/scripts/GameController.cs
+++ Assets/x_John_Assets/scripts/GameController.cs
@@ -17,6 +17,10 @@ public class GameController : MonoBehaviour {
 	public string link_id; // id of object containing Position/Rotation for player to start out in loaded scene.
 
 	public int reset_count = 0; // Keeping it public just to check in the inspector
+	public bool timer_activated = false;
+	public int timer_count = 0;
+	public int timer_max = 1000;
+	public float timer_rate = 1.0f;
 
 	void load_scene(string scene_name){
 
@@ -53,5 +57,20 @@ public class GameController : MonoBehaviour {
 		Transform starting_position = GameObject.Find (link_id).transform;
 		player.transform.position = starting_position.position;
 		player.transform.rotation = starting_position.rotation;
+		if (level > 1) {
+			if (!timer_activated) {
+				timer_activated = true;
+				StartCoroutine(Timer());
+			}
+		}
+	}
+
+	private IEnumerator Timer(int start_time = 0){
+		while (timer_count < timer_max) {
+			timer_count++;
+			yield return new WaitForSeconds (timer_rate);
+			Debug.Log (timer_count);
+		}
+		Debug.Log ("Timer has finished or was deactivated");
 	}
 }
